0d31bdab139adbd4a1eee641d3d42b7995e8a80c
sebastianbalestriere@hotmail.com <sebastianbalestriere@hotmail.com>
2021-06-12 16:02:14 -0300

    Data Project added to Solution, Grid control testing.

---
+++ SeaWideExpress/QuotingTool/AJC.Logistics.SeaWideExpress.QuotingTool.Data/Scripts/ScriptsIgnoredOnImport.sql
@@ -0,0 +1,13 @@
+﻿
+
+/*
+--SELECT * FROM [QuotingV2_UOMs]
+INSERT INTO [QuotingV2_UOMs]([Name],[Description],[DateAdded],[AddedBy]) VALUES('Weight','Total Weight (LB)',GetDate(),'SYSTEM');
+INSERT INTO [QuotingV2_UOMs]([Name],[Description],[DateAdded],[AddedBy]) VALUES('Volume','Total Volume (CuFT)',GetDate(),'SYSTEM');
+INSERT INTO [QuotingV2_UOMs]([Name],[Description],[DateAdded],[AddedBy]) VALUES('Volume (45LB)','Total Volume (CuFT w/45LB Rule)',GetDate(),'SYSTEM');
+INSERT INTO [QuotingV2_UOMs]([Name],[Description],[DateAdded],[AddedBy]) VALUES('Units','Unit Count',GetDate(),'SYSTEM');
+INSERT INTO [QuotingV2_UOMs]([Name],[Description],[DateAdded],[AddedBy]) VALUES('Units (Oversize)','Oversized Unit Count',GetDate(),'SYSTEM');
+*/
+
+
+GO
