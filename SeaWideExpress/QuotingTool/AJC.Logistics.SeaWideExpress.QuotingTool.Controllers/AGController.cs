83b2b6b9bd26f089a9190535cebff13ff47efd32
Ivan-WeTHINK <ivan@wethinklabs.com>
2021-06-16 20:55:19 -0400

    - Added new endpoint for UOMs. - Added column "Per" on the front-end with the wiring to the new endpoint.

--- SeaWideExpress/QuotingTool/AJC.Logistics.SeaWideExpress.QuotingTool.Controllers/AGController.cs
+++ SeaWideExpress/QuotingTool/AJC.Logistics.SeaWideExpress.QuotingTool.Controllers/AGController.cs
@@ -55,6 +55,7 @@ namespace AJC.Logistics.SeaWideExpress.QuotingTool.Controllers
                     city = fee.CityID,
                     zipCodes = fee.ZipCodes,
                     fee = fee.Value,
+                    uom = fee.ByUomID,
                     discount = fee.Discount,
                     feeMin = fee.FeeMin,
                     feeMax = fee.FeeMax,
@@ -127,6 +128,20 @@ namespace AJC.Logistics.SeaWideExpress.QuotingTool.Controllers
             using (QuotingToolRepository db = new QuotingToolRepository())
             {
                 var dataSet = db.Cities.Select(city => new { value = city.CityID, label = city.Name.Trim().ToUpper() }).ToList();
+                return Json(dataSet, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        /// <summary>
+        /// Get UOM entities for List of Values
+        /// </summary>
+        /// <returns></returns>
+        [System.Web.Mvc.HttpGet]
+        public JsonResult getUoms()
+        {
+            using (QuotingToolRepository db = new QuotingToolRepository())
+            {
+                var dataSet = db.UOMs.Select(uom => new { value = uom.UomID, label = uom.Name.Trim().ToUpper() }).ToList();
                 return Json(dataSet, JsonRequestBehavior.AllowGet);
             }
         }
