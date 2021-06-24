d7a61e6c6d5a25748eabebef56666cea3975b4dc
Ivan-WeTHINK <ivan@wethinklabs.com>
2021-06-23 20:24:04 -0400

    - Renamed models to match the views and controllers. - Added support to define filters on the FeesGrid View (filters are used to hide columns for now, but the concept was introduced for later use).

---
+++ SeaWideExpress/QuotingTool/AJC.Logistics.SeaWideExpress.QuotingTool.Business/FeesGridModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AJC.Logistics.SeaWideExpress.QuotingTool.Business
+{
+    public class FeesGridModel
+    {
+        public enum Mode {
+            Fees,
+            Ranges
+        }
+
+        public FeesGridModel(Mode GridMode)
+        {
+            this.GridMode = GridMode;
+            this.FilterBy = new List<string>();
+        }
+
+        public FeesGridModel(Mode GridMode, IList<string> FilterBy)
+        {
+            this.GridMode = GridMode;
+            this.FilterBy = FilterBy;
+        }
+
+        public Mode GridMode { get; set; }
+
+        public IList<string> FilterBy { get; set; }
+    }
+}
